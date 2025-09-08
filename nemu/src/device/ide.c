3ce9c67381a8720a756fe1ecefc03bf76ff35bf8
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-08 06:49:19 +0000

    >  compile NEMU 3023001461 root Linux 53f8d188db84 6.10.14-linuxkit #1 SMP PREEMPT_DYNAMIC Thu Aug 14 19:26:06 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  06:49:19 up 6 days, 19:39,  0 users,  load average: 0.66, 0.63, 0.47 aa68e5d4709b0dc643c9357075134a108c262c8e

--- nemu/src/device/ide.c
+++ nemu/src/device/ide.c
@@ -91,7 +91,7 @@ void bmr_io_handler(ioaddr_t addr, size_t len, bool is_write) {
 				/* DMA start command */
 				if(bmr_base[0] & 0x8) {
 					/* DMA read */
-					caches.std.refresh();
+					// caches.std.refresh();
 					/* the address of Physical Region Descriptor Table */
 					hwaddr_t prdt_addr = *(uint32_t *)(bmr_base + 4);
 
